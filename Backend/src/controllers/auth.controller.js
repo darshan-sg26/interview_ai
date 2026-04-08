ebfaef05ec83b759cbd37f2aa37209efe8f9966e
darshan-sg26 <darshansg46@gmail.com>
2026-04-08 17:56:30 +0530

    Restructured project into Folder

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const tokenBlacklistModel = require("../models/blacklist.model")
 
 
 /**
@@ -24,7 +25,7 @@ async function registerUserController(req,res){
         $or:[{username},{email}]
     })
 
-    if(isUserAlreadyExistsserAlreadyExists){
+    if(isUserAlreadyExists){
 
         return res.status(400).json({
             message:"Account already exists with this username or email address"
@@ -103,7 +104,49 @@ async function loginUserController(req,res){
 
 }
 
+/**
+ * @name logoutUserController
+ * @description clear cookie from user cookies and add the token in blacklist
+ * @access Public 
+ */
+async function logoutUserController(req,res){
+    const token = req.cookies.token
+
+    if(token){
+        await tokenBlacklistModel.create({token})
+    }
+
+    res.clearCookie("token") 
+
+    res.status(200).json({
+        message:"User logged out successfully"
+    })
+}
+
+/**
+ * @name getMeController
+ * @description get the current logged in user details
+ * @access Private
+ */
+
+async function getMeController(req,res){
+
+    const user = await userModel.findById(req.user.id)
+
+    res.status(200).json({
+        message:"User details fetched successfully",
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email
+        }
+    })
+
+}
+
 module.exports = {
     registerUserController,
-    loginUserController
+    loginUserController,
+    logoutUserController,
+    getMeController
 }
